2236336e722b0ce8e5053d1b0f1eaf12eb6731a7
Samarth <samarthdev138@gmail.com>
2023-06-10 21:57:53 -0700

    Kth factor of n

---
+++ KthFactorN.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+  public:
+    int kthFactor(int n, int k) {
+        vector<int> factors; 
+        int i = 1;
+        while (i <= n) {
+            if (n%i == 0) {
+                factors.push_back(i);
+            }
+            i++; 
+        }
+
+        if (k > factors.size()) {
+            return -1;
+        } else {
+            return factors[k-1]; 
+        }
+    }
+};
