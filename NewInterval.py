5655994ac3b6bd46ea7e11d2d23a8db98409a855
sdev138 <samarthdev138@gmail.com>
2024-09-29 18:43:20 -0700

    Solving problem 1804

--- NewInterval.py
+++ NewInterval.py
@@ -16,6 +16,7 @@ class Solution:
                 newInterval = [min(newInterval[0], intervals[i][0]), max(
                     newInterval[1], intervals[i][1])]
         res.append(newInterval)
+        # returning the result
         return res
 
 
---
+++ Problem1804.py
@@ -0,0 +1,64 @@
+class Trie:
+
+    def __init__(self):
+        self.storage = {}
+        self.fullList = []
+
+    def insert(self, word: str) -> None:
+        if word in self.storage:
+            self.storage[word] += 1
+            self.fullList.append(word)
+        else:
+            self.storage[word] = 1
+            self.fullList.append(word)
+
+    def countWordsEqualTo(self, word: str) -> int:
+        if word not in self.storage:
+            return 0
+        else:
+            return self.storage[word]
+
+    def countWordsStartingWith(self, prefix: str) -> int:
+        counter = 0
+        for word in self.fullList:
+            if prefix == word[:len(prefix)]:
+                counter += 1
+        return counter
+
+    def erase(self, word: str) -> None:
+        # removing the element from a map and all instances in a list
+        if word in self.storage and self.storage[word] > 0:
+            self.storage[word] -= 1
+            self.fullList.remove(word)
+            # while word in self.fullList:
+            #     self.fullList.remove(word)
+        return
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.countWordsEqualTo(word)
+# param_3 = obj.countWordsStartingWith(prefix)
+# obj.erase(word)
+
+def main():
+    obj = Trie()
+
+    word = "apple"
+    prefix = "app"
+
+    obj.insert(word)
+    obj.insert(word)
+
+    param2 = obj.countWordsEqualTo(word)
+    param3 = obj.countWordsStartingWith(prefix)
+    obj.erase(word)
+
+    solution = [2, 2]
+    output = [param2, param3]
+    if solution != output:
+        print("ERROR::::\nHere is your output: {}\n Here is the solution: {}".format(output, solution))
+    else:
+        print("Solution is correct!")
+
+main()
