3dc491a237e2ceeedadb283dd40cf4816f09d831
sdev138 <samarthdev138@gmail.com>
2024-10-21 23:50:00 -0700

    Finished problem 94

---
+++ Problem94.py
@@ -0,0 +1,12 @@
+# in order traversal of a binary tree
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        self.helper(root, result)
+        return result
+
+    def helper(self, root, result):
+        if root is not None:
+            self.helper(root.left, result)
+            result.append(root.val)
+            self.helper(root.right, result)
