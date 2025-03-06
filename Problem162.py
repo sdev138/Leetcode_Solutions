683076b349ec42b9eebf4e1720e3a9ce52f56a01
sdev138 <samarthdev138@gmail.com>
2025-03-05 20:58:14 -0800

    Editing peak application

--- Problem162.py
+++ Problem162.py
@@ -8,3 +8,20 @@ class Solution:
                 return i
         
         return 0
+
+    # better O(n) solution as a peak is simply greater than its neighbors not necessarily the greatest
+    def returnAPeak(self, nums: list[int]) -> int:
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                return i
+        
+        return len(nums) - 1
+
+    # binary search solution
+    def binarySearchPeak(self, nums: list[int]) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        # binary search
+        
+        return 0
